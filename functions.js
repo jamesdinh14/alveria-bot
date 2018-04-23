24c4802c576788d6bb7b6cae4a5cffd8f7ad1e27
James Dinh <jamesdinh14@gmail.com>
2018-04-23 13:58:47 -0400

    Add formatter for cleaner displays of argument lists

--- functions.js
+++ functions.js
@@ -21,9 +21,28 @@ module.exports = (client) => {
     return fs.readFileSync(inputFileName).toString().split('\n');
   };
 
+  // Send the desired message to the current channel
   client.sendMessage = (message, messageToBeSent) => {
     message.channel.send(messageToBeSent);
   };
+
+  // Form a string that is correctly displayed based on the number of arguments
+  client.formatArguments = (inputArray) => {
+    let outString = "";
+
+    // Insert "and" at the 2nd to last position if there is more than 1 argument
+    if (inputArray.length > 1) {
+      inputArray.splice(-1, 1, `and ${inputArray[inputArray.length - 1]}`);
+
+      // Insert commas or join two arguments
+      if (inputArray.length > 2) {
+        outString = inputArray.join(", ");
+      } else {
+        outString = inputArray.join(" ");
+      }
+    }
+    return outString;
+  }
 
   // Courtesy of guidebot
   // <Array>.random() returns a single random element from an array
