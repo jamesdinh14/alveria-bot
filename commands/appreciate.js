8c0709bd7e7e871a06535f3323cb760542b128fc
MenjiShad <jamesdinh14@gmail.com>
2018-04-23 21:47:13 -0400

    Remove newline from appreciate message

--- commands/appreciate.js
+++ commands/appreciate.js
@@ -14,13 +14,11 @@ exports.run = (client, message, args) => {
         });
 
         let mentionedNamesFormatted = client.formatArguments(mentionedNames);
-        let messageSend = `${message.member.nickname? message.member.nickname : message.member.user.username} 
-            appreciates ${mentionedNamesFormatted}!`;
+        let messageSend = `${message.member.nickname? message.member.nickname : message.member.user.username} appreciates ${mentionedNamesFormatted}!`;
         client.sendMessage(message, messageSend);
     }
     else if (args.length !== 0) {
-        let messageSend = (`${message.member.nickname? message.member.nickname : message.member.user.username} 
-            appreciates ${args.join(" ")}!`);
+        let messageSend = (`${message.member.nickname? message.member.nickname : message.member.user.username} appreciates ${args.join(" ")}!`);
         client.sendMessage(message, messageSend);
     }
     else {
